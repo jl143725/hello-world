4d88a499e935c44d40217c06fb7f968623e2d00b
Joseph Lagbenro <jlagb>
2020-03-04 10:22:26 +0000

    added unit project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+
+namespace TestAutomationTask
+{
+    [TestClass]
+    public class Access_JDAccount;
+    
+
+
+
+
+
+    
+
+
+
+}
+
+
